510526cfea0f2e97c59e4a84e4f373cfba5bdda4
EsnaNurYlmz <esnanuryilmaz407@gmail.com>
2024-12-09 00:57:40 +0300

    Initial Commit

---
+++ CepteBilet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CepteBilet
+//
+//  Created by Esna nur Yılmaz on 9.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CepteBiletTests/CepteBiletTests.swift
@@ -0,0 +1,17 @@
+//
+//  CepteBiletTests.swift
+//  CepteBiletTests
+//
+//  Created by Esna nur Yılmaz on 9.12.2024.
+//
+
+import Testing
+@testable import CepteBilet
+
+struct CepteBiletTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
