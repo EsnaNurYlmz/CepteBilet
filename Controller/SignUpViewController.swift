045060e279271302b442779c00a0d80c07f8085b
EsnaNurYlmz <esnanuryilmaz407@gmail.com>
2024-12-09 16:17:12 +0300

    SignUpViewController design created

---
+++ Controller/SignUpViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SignUpViewController.swift
+//  CepteBilet
+//
+//  Created by Esna nur Yılmaz on 9.12.2024.
+//
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    
+
+}
