9699a9c489fb0431db6e388ea5690b0b00799d74
EsnaNurYlmz <esnanuryilmaz407@gmail.com>
2024-12-09 15:51:41 +0300

    LoginViewController design created

---
+++ Controller/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  CepteBilet
+//
+//  Created by Esna nur Yılmaz on 9.12.2024.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+
+}
