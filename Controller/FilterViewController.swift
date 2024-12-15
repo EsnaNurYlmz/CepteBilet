bb571ed3fed6313df7dc324c1205a7c936d48985
EsnaNurYlmz <esnanuryilmaz407@gmail.com>
2024-12-16 01:24:29 +0300

    FilterViewController and design created.

---
+++ Controller/FilterViewController.swift
@@ -0,0 +1,21 @@
+//
+//  FilterViewController.swift
+//  CepteBilet
+//
+//  Created by Esna nur Yılmaz on 12.12.2024.
+//
+
+import UIKit
+
+class FilterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+
+}
