12fe99a95d322bf1628f42909a193e7d16cb6289
EsnaNurYlmz <esnanuryilmaz407@gmail.com>
2024-12-17 00:49:56 +0300

    DetailViewController design and TicketsViewController created

---
+++ Controller/TicketsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  TicketsViewController.swift
+//  CepteBilet
+//
+//  Created by Esna nur Yılmaz on 16.12.2024.
+//
+
+import UIKit
+
+class TicketsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
