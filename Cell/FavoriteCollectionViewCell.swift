604d284d1d20ac56fd8a7f9a92ae9fd8165df101
EsnaNurYlmz <esnanuryilmaz407@gmail.com>
2024-12-16 16:56:53 +0300

    FavoriteViewController and design created

---
+++ Cell/FavoriteCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  FavoriteCollectionViewCell.swift
+//  CepteBilet
+//
+//  Created by Esna nur Yılmaz on 16.12.2024.
+//
+
+import UIKit
+
+class FavoriteCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Controller/FavoriteViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FavoriteViewController.swift
+//  CepteBilet
+//
+//  Created by Esna nur Yılmaz on 16.12.2024.
+//
+
+import UIKit
+
+class FavoriteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+}
