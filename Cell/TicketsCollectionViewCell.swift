335b38252f1b0feb192526347d3dc1e4d353a150
EsnaNurYlmz <esnanuryilmaz407@gmail.com>
2024-12-17 15:48:36 +0300

    TicketsViewController  design created

---
+++ Cell/TicketsCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  TicketsCollectionViewCell.swift
+//  CepteBilet
+//
+//  Created by Esna nur Yılmaz on 17.12.2024.
+//
+
+import UIKit
+
+class TicketsCollectionViewCell: UICollectionViewCell {
+    
+}
