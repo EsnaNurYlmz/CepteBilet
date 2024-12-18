b00c45659c55e930ac19125815ba6a70eaa623da
EsnaNurYlmz <esnanuryilmaz407@gmail.com>
2024-12-18 12:45:29 +0300

    DetailViewController design update and ReviewTableViewCell created

---
+++ Cell/ReviewTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ReviewTableViewCell.swift
+//  CepteBilet
+//
+//  Created by Esna nur Yılmaz on 18.12.2024.
+//
+
+import UIKit
+
+class ReviewTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Controller/ReviewViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ReviewViewController.swift
+//  CepteBilet
+//
+//  Created by Esna nur Yılmaz on 18.12.2024.
+//
+
+import UIKit
+
+class ReviewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
