0e5f08953876401a10de2218180e8c55ac358a9e
EsnaNurYlmz <esnanuryilmaz407@gmail.com>
2024-12-27 01:17:29 +0300

    TicketsBuyVievControlller and TicketsViewController design created

--- Controller/ReviewViewController.swift
+++ Controller/ReviewViewController.swift
@@ -12,7 +12,7 @@ class ReviewViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
     
 
---
+++ Controller/TicketsBuyViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TicketsBuyViewController.swift
+//  CepteBilet
+//
+//  Created by Esna nur Yılmaz on 26.12.2024.
+//
+
+import UIKit
+
+class TicketsBuyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+}
