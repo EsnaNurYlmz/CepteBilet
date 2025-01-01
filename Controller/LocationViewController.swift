7a90ccb3dc9dac94eba40fbb6961186627c72176
EsnaNurYlmz <esnanuryilmaz407@gmail.com>
2025-01-02 01:40:18 +0300

    LocationViewController and design created

---
+++ Controller/LocationViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LocationViewController.swift
+//  CepteBilet
+//
+//  Created by Esna nur Yılmaz on 2.01.2025.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class LocationViewController: UIViewController {
+
+    @IBOutlet weak var MapView: MKMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+
+}
